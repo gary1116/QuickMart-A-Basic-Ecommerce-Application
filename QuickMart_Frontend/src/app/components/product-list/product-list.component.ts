232377a5eee5c11cc702e2a5ceefa9fa6b6615e9
gary1116 <garygodwin234@gmail.com>
2023-08-25 16:26:43 +0530

    worked on pagination and searching pagination and added a dropdown for page size

--- QuickMart_Frontend/src/app/components/product-list/product-list.component.ts
+++ QuickMart_Frontend/src/app/components/product-list/product-list.component.ts
@@ -11,7 +11,17 @@ import { ProductService } from 'src/app/service/product.service';
 export class ProductListComponent implements OnInit {
   products: Product[] = [];
   currentCategoryId: number = 1;
+  previousCategoryId: number=1;
   searchMode: boolean = false;
+
+
+  // new properties for pagination
+
+  thePageNumber:number=1;
+  thePageSize:number=5;
+  theTotalElements:number=0;
+
+previousKeyword: string = "";
 
   constructor(private productService: ProductService,
     private route: ActivatedRoute) { }
@@ -34,13 +44,17 @@ export class ProductListComponent implements OnInit {
   handleSearchProducts(){
     const theKeyword: string = this.route.snapshot.paramMap.get('keyword')!;
 
-    // search for products using that given keyword
+if(this.previousKeyword != theKeyword){
+  this.thePageNumber=1;
+}
 
-    this.productService.searchProducts(theKeyword).subscribe(
-      data=>{
-        this.products=data;
-      }
-    )
+this.previousKeyword = theKeyword;
+console.log(`keyword = ${theKeyword}, the page number = ${this.thePageNumber}`);
+
+    // search for products using that given keyword
+    this.productService.searchProductsPaginate(this.thePageNumber-1,
+                                               this.thePageSize,
+                                               theKeyword).subscribe(this.processResult());
   }
 
   handleListProducts() {
@@ -55,12 +69,42 @@ export class ProductListComponent implements OnInit {
       this.currentCategoryId = 1;
     }
 
-    this.productService.getProductList(this.currentCategoryId).subscribe(
-      data => {
-        this.products = data;
-      }
-    )
+    // 
+    // check if we hae a different category than the previous
+    // Note: Angular will reuse a component if it is currently being viewed
+
+    // if we have a different category id than previous
+    // then we want to reset thePageNumber back to 1
+    if(this.previousCategoryId != this.currentCategoryId){
+      this.thePageNumber=1;
+    }
+
+    this.previousCategoryId=this.currentCategoryId;
+    
+    console.log(`current categiry id = ${this.currentCategoryId}, thePageNumber=${this.thePageNumber}`);
+
+
+    this.productService.getProductListPaginate(this.thePageNumber - 1,
+                                                this.thePageSize,
+                                                this.currentCategoryId)
+                                                .subscribe(this.processResult());
   }
+
+  updatePageSize(pageSize: string){
+this.thePageSize= +pageSize;
+this.thePageNumber=1;
+this.listProducts();
+  }
+
+  processResult(){
+    return(data:any)=>{
+      this.products=data._embedded.products;
+      this.thePageNumber=data.page.number+1;
+      this.thePageSize=data.page.size;
+      this.theTotalElements=data.page.totalElements;
+    };
+  }
+
 }
 
 
--- QuickMart_Frontend/src/app/service/product.service.ts
+++ QuickMart_Frontend/src/app/service/product.service.ts
@@ -15,20 +15,30 @@ export class ProductService {
   private categoryUrl = "http://localhost:8080/api/product-category";
 
   constructor(private httpClient: HttpClient) { }
-
+// =========================
   getProduct(theProductId: number):Observable<Product>{
     // need to build a url based on product id
     const productUrl = `${this.baseUrl}/${theProductId}`;
     
     return this.httpClient.get<Product>(productUrl);
   }
-
+// -----------------------
   getProductList(theCategoryId:number): Observable<Product[]> {
 
     const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`;   
     return this.getProducts(searchUrl);
   }
+// --------------------------------
+  getProductListPaginate(thePage:number,
+                         thePageSize: number,
+                         theCategoryId:number): Observable<GetResponseProducts> {
 
+    // need to build a url based on category id, page and size
+    const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`
+                        + `&page=${thePage}&size=${thePageSize}`;   
+    return this.httpClient.get<GetResponseProducts>(searchUrl);
+  }
+// ---------------------------
   searchProducts(theKeyword:string): Observable<Product[]>{
     //  need to build a url based on the keyword
       const searchUrl = `${this.baseUrl}/search/findByNameContaining?name=${theKeyword}`;   
@@ -37,24 +47,42 @@ export class ProductService {
   
     }
 
+// -------------------------------
 
+searchProductsPaginate(thePage:number,
+                      thePageSize: number,
+                      theKeyword:string): Observable<GetResponseProducts> {
+
+// need to build a url based on keyword, page and size
+const searchUrl = `${this.baseUrl}/search/findByNameContaining?name=${theKeyword}`
+                    + `&page=${thePage}&size=${thePageSize}`;   
+return this.httpClient.get<GetResponseProducts>(searchUrl);
+}
+
+// --------------------------------
   getProductCategories(): Observable<ProductCategory[]> {
    
     return this.httpClient.get<GetResponseProductCategory>(this.categoryUrl).pipe(
       map(response => response._embedded.productCategory)
     );
   }
-
+// ---------------------------------
   private getProducts(searchUrl: string): Observable<Product[]> {
     return this.httpClient.get<GetResponseProducts>(searchUrl).pipe(
       map(response => response._embedded.products)
     );
   }
 }
-
+// ========================================
 interface GetResponseProducts {
   _embedded: {
     products: Product[];
+  },
+  page: {
+    size: number,
+    totalElements: number,
+    totalPages: number,
+    number:number
   }
 }
 
