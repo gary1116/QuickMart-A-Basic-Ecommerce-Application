b52e8ab0094dd029b707cc6000437caea5145d84
gary1116 <garygodwin234@gmail.com>
2023-08-30 21:10:44 +0530

    worked on validations of each section for checkout page

---
+++ QuickMart_Frontend/src/app/validators/quick-mart-validators.spec.ts
@@ -0,0 +1,7 @@
+import { QuickMartValidators } from './quick-mart-validators';
+
+describe('QuickMartValidators', () => {
+  it('should create an instance', () => {
+    expect(new QuickMartValidators()).toBeTruthy();
+  });
+});
