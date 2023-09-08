2155b2addcda1a24485ba8191c3a8ff01d25606c
gary1116 <garygodwin234@gmail.com>
2023-09-08 11:33:41 +0530

    added login/logout feature through OKTA Authentication

---
+++ QuickMart_Frontend/src/app/config/my-app-config.ts
@@ -0,0 +1,22 @@
+export default  {
+
+    oidc:{
+        clientId: '0oab3ldtwdVSWjbhh5d7',
+        issuer: 'https://dev-43432448.okta.com/oauth2/default',
+        redirectUri: 'http://localhost:4200/login/callback',
+        scopes: ['openid','profile','email']
+    }
+}
+
+// client id is the public identifier of the client app
+
+// issuer: is the issuer of tokens thats the url we will use when authorizing with
+// okta authorization server
+
+// redirect uri is where they gonna send the user once the user logs in
+
+// scopes provide access to information about a user
+
+// openid: required for authentication requests
+// profile: user's first name, last name, phone,etc
+// email: user's email address
